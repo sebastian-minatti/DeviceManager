9d804b1a25782e3e77e3957b9b20751d219996da
argjv <valdeiglesiasj@gmail.com>
2015-07-10 00:41:25 -0700

    Added navigation between pages, new basic add forms, basic tables, dashboard graph.

--- grails-app/assets/javascripts/routingScript.js
+++ grails-app/assets/javascripts/routingScript.js
@@ -18,62 +18,113 @@ glabApp.config(function($routeProvider) {
 		templateUrl : 'pages/devices.html',
 		controller : 'devicesController'
 	})
-	
+
 	.when('/history', {
 		templateUrl : 'pages/history.html',
 		controller : 'historyController'
 	})
-	
+
 	.when('/monitoring', {
 		templateUrl : 'pages/monitoring.html',
 		controller : 'monitoringController'
 	})
-	
+
 	.when('/external', {
 		templateUrl : 'pages/external.html',
 		controller : 'externalController'
 	})
-	
+
 	.when('/reminders', {
 		templateUrl : 'pages/reminders.html',
 		controller : 'remindersController'
 	})
-	
-	.when('/sendreminder', {
-		templateUrl : 'pages/sendreminder.html',
+
+	.when('/sendReminder', {
+		templateUrl : 'pages/sendReminder.html',
 		controller : 'sendreminderController'
+	})
+
+	.when('/addDevice', {
+		templateUrl : 'pages/addDevice.html',
+		controller : 'addDeviceController'
 	});
 });
 
 // Controllers
 glabApp.controller('dashboardController', function($scope) {
 	$scope.message = 'Dashboard UI';
+	$scope.data = [ {
+		y : '21',
+		a : 0,
+		b : 0
+	}, {
+		y : '22',
+		a : 1,
+		b : 0
+	}, {
+		y : '23',
+		a : 2,
+		b : 0
+	}, {
+		y : '24',
+		a : 0,
+		b : 1
+	}, {
+		y : '25',
+		a : 1,
+		b : 0
+	}, {
+		y : '26',
+		a : 0,
+		b : 0
+	}, {
+		y : '27',
+		a : 100,
+		b : 90
+	}, {
+		y : '28',
+		a : 2,
+		b : 0
+	} ];
 });
 
-glabApp.controller('borrowController', function($scope) {
-	$scope.message = 'Borrow a device UI.';
-});
+glabApp
+		.controller(
+				'borrowController',
+				function($scope) {
+					$scope.message = 'Form to register a device that was lend to someone outside to the team.';
+				});
 
 glabApp.controller('devicesController', function($scope) {
 	$scope.message = 'List of all the devices with different filters.';
 });
 
-glabApp.controller('historyController', function($scope) {
-	$scope.message = 'Device transaction history';
-});
+glabApp
+		.controller(
+				'historyController',
+				function($scope) {
+					$scope.message = 'Table with all the devices borrowed to other people/team and also the ones we asked for.';
+				});
 
 glabApp.controller('monitoringController', function($scope) {
 	$scope.message = 'Device monitoring UI.';
 });
 
-glabApp.controller('externalController', function($scope) {
-	$scope.message = 'List of all the external devices with different filters.';
-});
+glabApp
+		.controller(
+				'externalController',
+				function($scope) {
+					$scope.message = 'Form to register an external device borrowed to the team.';
+				});
 
 glabApp.controller('remindersController', function($scope) {
 	$scope.message = 'List of all the reminders sent.';
 });
 
-glabApp.controller('sendreminderController', function($scope) {
+glabApp.controller('sendReminderController', function($scope) {
 	$scope.message = 'Send a reminder UI.';
+});
+
+glabApp.controller('addDeviceController', function($scope) {
+	$scope.message = 'Form to add a new Device';
 });
